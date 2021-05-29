8c09724c67465b58205848abe596df3121217a1b
Severino-ferreira <79754061+Severino-ferreira@users.noreply.github.com>
2021-05-29 17:07:47 -0300

    Add files via upload

---
+++ mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package br.com.alura.mvc.mudi;
+
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    private DataSource dataSource;
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/home/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin(form->form
+                .loginPage("/login").defaultSuccessUrl("/usuario/pedido",true)
+                .permitAll())
+            .logout(logout ->logout
+                    .logoutUrl("/logout"));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        
+        /*UserDetails user =
+                User.builder()
+                    .username("reir")
+                   .password(encoder.encode("1234"))
+                   .roles("ADM")
+                   .build();*/
+        
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .passwordEncoder(encoder);
+            //.withUser(user);
+        
+    }
+}   
